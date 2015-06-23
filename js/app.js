83f57d58de77f31fce2f7eeb89c96d71d7974a17
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-23 16:02:15 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var color = 'green';
+var waypoint = new Waypoint({
+  element: document.querySelector('.three'),
+  handler: function(direction) {
+    var nav = document.querySelector('nav');
+    var background = nav.style.background;
+    if (direction === 'up') {
+      if (background === 'green') {
+        nav.style.background = 'orange';
+      } else {
+        nav.style.background = 'green';
+      }
+    }
+  }
+});
+
+var imageWaypoint = new Waypoint({
+  element: document.querySelector('.image'),
+  handler: function(direction) {
+    var nav = document.querySelector('nav');
+    if (direction === 'down') {
+      nav.style.background = 'red';
+    }
+  },
+  offset: 'bottom-in-view'
+});
