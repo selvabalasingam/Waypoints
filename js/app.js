65f0f3d99f4287a6c0cc39581034ab53c07d9c4d
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-24 17:15:55 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
+// NOTE: There is no styling for the color of the nagivation bar in CSS, it's done here instead.
+
 $(document).ready(function(){
-  var color = 'green';
   var waypoint = new Waypoint({
   element: document.querySelector('.five'),
   handler: function(direction) {
     var nav = document.querySelector('nav');
     var background = nav.style.background;
     if (direction === 'down') {
-      if (background === 'green') {
-        nav.style.background = 'orange';
-      } else {
         nav.style.background = 'green';
-      }
+    }
+    if (direction === 'up') {
+      if (background === 'green') {
+        nav.style.background = 'red'; 
+      } 
     }
   }
   });
 
+  // The code below loads the navigation bar as red from the get-go.
   var imageWaypoint = new Waypoint({
-  element: document.querySelector('.image'),
+  element: document.querySelector('.five'),
   handler: function(direction) {
     var nav = document.querySelector('nav');
     if (direction === 'down') {
-      nav.style.background = 'red';
+      nav.style.background = 'red'; 
     }
   },
   offset: 'bottom-in-view'
-});
+  }); 
+
+
 });
